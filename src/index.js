62738e5d474b9152213d1f426bf472685738034b
Bruno Bortagaray <bruno.bortagaray@gmail.com>
2017-01-22 22:09:30 -0200

    Few more changes

--- src/index.js
+++ src/index.js
@@ -5,23 +5,37 @@ const app = new Koa();
 
 const TweetClient = new Twit(config);
 
+// This variable will show the magic!
+let dataTweet = '';
 
 // Tweeting things
-let tweet = {
-	status: 'One last try... #coding'
-}
-let dataTweet = '';
-TweetClient.post('statuses/update', tweet, async function(err, data, response) {
+
+/* let tweet = {
+// 	status: 'One last try... #coding'
+// }
+
+// TweetClient.post('statuses/update', tweet, async function(err, data, response) {
+// 	if(err) {
+// 		console.log('An Error ocurred: ' + err.message);
+// 	} else {
+// 		console.log('It worked! The tweet is: ' + data.text);
+// 	}
+// 	dataTweet = data;
+// });
+*/
+
+// Just find this on ww trends
+TweetClient.get('search/tweets', { q: 'the Packers', count: 25 }, function(err, data, response) {
 	if(err) {
 		console.log('An Error ocurred: ' + err.message);
 	} else {
-		console.log('It worked! The tweet is: ' + data.text);
-	}
-	dataTweet = data;
+		console.log('It worked! Here\'s your search: ' + JSON.stringify(data, null, 2));
+ 	}
+  dataTweet = data;
 });
 
-app.use(ctx => {
-	ctx.body = dataTweet;
+app.use(async ctx => {
+	ctx.body = JSON.stringify(dataTweet, null, 2);
 });
 
 app.listen(3000, () => {
